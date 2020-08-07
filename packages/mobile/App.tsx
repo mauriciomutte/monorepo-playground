cf17f9d493afe50becab5e65b3c5035915c10a1e
Luis Filipe Alves de Oliveira <macbook-pro-lf-oliveira@MacBook-Pro-de-Luis.local>
2020-08-07 12:42:23 -0300

    feat: add react-native-cli

---
+++ packages/mobile/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+
+      <Text style={styles.text}>Hello monorepo!</Text>
+
+      <SafeAreaView />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    fontSize: 27,
+  },
+});
+
+export default App;
